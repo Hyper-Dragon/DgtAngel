1916b4cd4e43fbb3cbbdff0c0ca094775380cb31
Hyper-Dragon <61525854+Hyper-Dragon@users.noreply.github.com>
2021-08-02 18:44:00 +0100

    Generating correct FEN from the 'live' board

--- source/DgtAngel/wwwroot/Scripts/HtmlScrape.js
+++ source/DgtAngel/wwwroot/Scripts/HtmlScrape.js
@@ -8,10 +8,63 @@ function modifyDOM() {
     return document.body.innerHTML;
 }
 
-function piecesFromDOM() {
-    //You can play with your DOM here or check URL against your regex
-    console.log(document.getElementsByClassName('piece'));
-    return document.getElementsByClassName('piece').toString();
+function piecesFromLiveBoardDOM() {
+    pieces = document.getElementsByClassName('piece');
+    myPiecesArray = Array.from(pieces);
+    piecesStringOut = myPiecesArray.map(o => o.style.backgroundImage.split('/').pop().substring(0, 2) + o.className.replace("piece square-", "").replaceAll("0", "")).join(',');
+
+    whiteClock = document.getElementsByClassName("clock-white")[0].innerText;
+    blackClock = document.getElementsByClassName("clock-black")[0].innerText;
+
+    retVal = whiteClock + '|' + blackClock + '|' + piecesStringOut;
+
+    console.log('Returning: ' + retVal);
+
+    return retVal;
+}
+
+function piecesFromPlayBoardDOM() {
+    pieces = document.getElementsByClassName('piece');
+    myPiecesArray = Array.from(pieces);
+    piecesStringOut = myPiecesArray.map(o => o.className.replace("piece ", "").replace(" square-", "")).join(',');
+
+    whiteClock = document.getElementsByClassName("clock-white")[0].innerText;
+    blackClock = document.getElementsByClassName("clock-black")[0].innerText;
+
+    retVal = whiteClock + '|' + blackClock + '|' + piecesStringOut;
+
+    //console.log('Returning: ' + retVal);
+
+    return retVal;
+}
+
+async function getPiecesHtml() {
+
+    retVal = '-';
+
+    try {
+        //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
+        await chrome.tabs.executeScript({ code: '(' + piecesFromLiveBoardDOM + ')();' },
+            (results) => {
+                retVal = results[0];
+                //console.log('Inside: ' + retVal);
+            });
+
+
+        //Sleep in loop
+        for (let i = 0; i < 5; i++) {
+            if (retVal != '-')
+                break;
+
+            //Need to wait for the script result 
+            await sleep(500);
+        }
+    } catch(ex) {
+        return 'ERROR:'+ex;
+    }
+
+    console.log('Outside: ' + retVal);
+    return retVal;
 }
 
 function getPageHtml() {
@@ -25,39 +78,12 @@ function getPageHtml() {
     });
 }
 
-function getPiecesHtml() {
-    //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
-    chrome.tabs.executeScript({
-        code: '(' + piecesFromDOM + ')();' //argument here is a string but function.toString() returns function's code
-    }, (results) => {
-        //Here we have just the innerHTML and not DOM structure
-        console.log('Pieces:')
-        console.log(results[0]);
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
     });
 }
 
-function getWhiteClockHtml() {
-    //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
-    chrome.tabs.executeScript({
-        code: '(return document.getElementsByClassName("clock-white")[0].innerText)();' //argument here is a string but function.toString() returns function's code
-    }, (results) => {
-        //Here we have just the innerHTML and not DOM structure
-        console.log('Popup script:')
-        console.log(results[0]);
-    });
+function writeToConsole(msg) {
+    console.log('Blazor:'+msg)
 }
-
-function getBlackClockHtml() {
-    //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
-    chrome.tabs.executeScript({
-        code: '(return document.getElementsByClassName("clock-black")[0].innerText)();' //argument here is a string but function.toString() returns function's code
-    }, (results) => {
-        //Here we have just the innerHTML and not DOM structure
-        console.log('Popup script:')
-        console.log(results[0]);
-    });
-}
-
-
-
-//});
--- source/DgtAngelLib/ChessDotComHelpers.cs
+++ source/DgtAngelLib/ChessDotComHelpers.cs
@@ -13,6 +13,39 @@ namespace DgtAngelLib
         //
         //}
 
+        // 2:58|2:57|br88,bn78,bb68,bk58,bq48,bb38,bn36,br18,bp87,bp77,bp67,bp57,bp47,bp35,bp27,bp17,wp82,wp72,wp62,wp54,wp42,wp32,wp22,wp12,wr81,wn63,wb61,wk51,wq41,wb31,wn21,wr11
+        public string ConvertHtmlToFenT2(string htmlIn)
+        {
+            ChessDotNet.Piece[][] board = new ChessDotNet.Piece[8][];
+            for (int loop = 0; loop < 8; loop++)
+            {
+                board[loop] = new ChessDotNet.Piece[8];
+            }
+
+            string[] gameString = htmlIn.ToLowerInvariant().Split('|');
+
+
+            foreach (string sqr in gameString[2].Split(','))
+            {
+                board[7 - (int.Parse(sqr[3].ToString())-1)][int.Parse(sqr[2].ToString())-1] = sqr[1] switch
+                {
+                    'p' => new ChessDotNet.Pieces.Pawn(((sqr[0] == 'w') ? ChessDotNet.Player.White : ChessDotNet.Player.Black)),
+                    'n' => new ChessDotNet.Pieces.Knight(((sqr[0] == 'w') ? ChessDotNet.Player.White : ChessDotNet.Player.Black)),
+                    'b' => new ChessDotNet.Pieces.Bishop((sqr[0] == 'w') ? ChessDotNet.Player.White : ChessDotNet.Player.Black),
+                    'r' => new ChessDotNet.Pieces.Rook(((sqr[0] == 'w') ? ChessDotNet.Player.White : ChessDotNet.Player.Black)),
+                    'k' => new ChessDotNet.Pieces.King(((sqr[0] == 'w') ? ChessDotNet.Player.White : ChessDotNet.Player.Black)),
+                    'q' => new ChessDotNet.Pieces.Queen(((sqr[0] == 'w') ? ChessDotNet.Player.White : ChessDotNet.Player.Black)),
+                    _ => throw new Exception("Unknown Piece Type")
+                };
+            }
+
+            return new ChessDotNet.ChessGame(new ChessDotNet.GameCreationData()
+            {
+                WhoseTurn = ChessDotNet.Player.White,
+                Board = board,
+            }).GetFen();
+        }
+
 
         /// <summary>
         /// Turns the output of document.getElementsByClassName('piece')
@@ -26,7 +59,7 @@ namespace DgtAngelLib
         ///div.piece.wk.square-62, div.piece.wr.square-86] 
         /// </param>
         /// <returns>Fen String</returns>
-        public string ConvertHtmlToFen(string htmlIn)
+        public string ConvertHtmlToFenT1(string htmlIn)
         {
             ChessDotNet.Piece[][] board = new ChessDotNet.Piece[8][];
             for (int loop = 0; loop < 8; loop++)
