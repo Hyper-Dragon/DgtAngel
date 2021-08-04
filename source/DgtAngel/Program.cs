cfc5b239260b49f6510646a4ce0bb9a6f5f35cda
Hyper-Dragon <61525854+Hyper-Dragon@users.noreply.github.com>
2021-08-04 17:57:48 +0100

    Interface switch done

--- source/DgtAngel/Program.cs
+++ source/DgtAngel/Program.cs
@@ -19,16 +19,19 @@ namespace DgtAngel
             // workaround to use JavaScript fetch to bypass url validation
             // see: https://github.com/dotnet/runtime/issues/52836
             builder.Services.AddScoped<HttpClient>(sp => new JsHttpClient(sp) { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddSingleton<Services.AppData>();
-            builder.Services.AddSingleton(sp => new ChessDotComHelpers());
-            builder.Services.AddTransient(sp => new ScriptWrapper(sp.GetService<IJSRuntime>()));
-            builder.Services.AddTransient(sp => new ChessDotComWatcher(sp.GetService<ScriptWrapper>(), sp.GetService<Services.AppData>(), sp.GetService<IChessDotComHelpers>()));            
-            builder.Services.AddTransient(sp => new DgtLiveChess());
+            
+            builder.Services.AddSingleton<IAppData, AppData>();
+            builder.Services.AddSingleton<IChessDotComHelpers,ChessDotComHelpers>();
+            
+            builder.Services.AddTransient<IScriptWrapper, ScriptWrapper>(sp => new ScriptWrapper(sp.GetService<IJSRuntime>()));
+            builder.Services.AddTransient<IChessDotComWatcher, ChessDotComWatcher>(sp => new ChessDotComWatcher(sp.GetService<IScriptWrapper>(), sp.GetService<IAppData>(), sp.GetService<IChessDotComHelpers>()));            
+            builder.Services.AddTransient<IDgtLiveChess, DgtLiveChess>(sp => new DgtLiveChess());
 
             builder.Services.AddBrowserExtensionServices(options =>
             {
                 options.ProjectNamespace = typeof(Program).Namespace;
             });
+
             await builder.Build().RunAsync();
         }
     }
