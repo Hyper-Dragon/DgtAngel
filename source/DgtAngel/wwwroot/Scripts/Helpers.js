1ca61a078f19be913b202dcf6e25dbacdce8ba7e
Hyper-Dragon <61525854+Hyper-Dragon@users.noreply.github.com>
2021-08-06 00:47:44 +0100

    Audio tracks are now queued (they don't play over the top of each other)

--- source/DgtAngel/wwwroot/Scripts/Helpers.js
+++ source/DgtAngel/wwwroot/Scripts/Helpers.js
@@ -1,6 +1,52 @@
-﻿//document.getElementById("test").addEventListener('click', () => {
-//    console.log("Popup DOM fully loaded and parsed");
+﻿// ********************************************************************************
+//       ___                       _        _ _       _
+//      /  _>  ___ ._ _  ___  _ _ <_> ___  | | | ___ | | ___  ___  _ _  ___
+//      | <_/\/ ._>| ' |/ ._>| '_>| |/ | ' |   |/ ._>| || . \/ ._>| '_><_-<
+//      `____/\___.|_|_|\___.|_|  |_|\_|_. |_|_|\___.|_||  _/\___.|_|  /__/
+//                                                      |_|
+// ********************************************************************************
 
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+
+function Queue() {
+    this.elements = [];
+}
+
+Queue.prototype.enqueue = function (e) {
+    this.elements.push(e);
+};
+
+// remove an element from the front of the queue
+Queue.prototype.dequeue = function () {
+    return this.elements.shift();
+};
+
+// check if the queue is empty
+Queue.prototype.isEmpty = function () {
+    return this.elements.length == 0;
+};
+
+// get the element at the front of the queue
+Queue.prototype.peek = function () {
+    return !this.isEmpty() ? this.elements[0] : undefined;
+};
+
+Queue.prototype.length = function () {
+    return this.elements.length;
+}
+
+// ********************************************************************************
+//                  ___    _                _
+//                 / __> _| |_  ___  _ _  _| |_  _ _  ___
+//                 \__ \  | |  <_> || '_>  | |  | | || . \
+//                 <___/  |_|  <___||_|    |_|  `___||  _/
+//                                                   |_|
+// ********************************************************************************
 
 function addIndexToContextMenu() {
     chrome.contextMenus.removeAll();
@@ -13,19 +59,52 @@ function addIndexToContextMenu() {
     });
 }
 
+// ********************************************************************************
+//             ___         _  _        ___  _
+//            | . | _ _  _| |<_> ___  | . \| | ___  _ _  ___  _ _
+//            |   || | |/ . || |/ . \ |  _/| |<_> || | |/ ._>| '_>
+//            |_|_|`___|\___||_|\___/ |_|  |_|<___|`_. |\___.|_|
+//                                                 <___'
+// ********************************************************************************
+
+let audioPlaylist = new Queue();
+let isAudioPlaying = false;
+let areAudioEventListenersAdded = false;
+
 function playAudioFromBkg(audioId) {
-    document.getElementById(audioId).play();
+    //Can't setup the Event Listener in background.html so we have to do it here (security error)
+    if (!areAudioEventListenersAdded) {
+        allAudioTags = Array.from(document.getElementsByTagName('audio'));
+        allAudioTags.forEach(item => item.addEventListener('ended', function () { playNext(); }));
+    }
+
+    //add the requested file to the playlist
+    audioPlaylist.enqueue(audioId);
+
+    //if no audio is playing then start
+    //if audio is playing the next track will play automatically
+    if (!isAudioPlaying) {
+        isAudioPlaying = true;
+        playNext();
+    }
 }
 
-function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
+function playNext() {
+    //if there are more tracks to play keep playing - otherwise flag audio as stopped
+    if (!audioPlaylist.isEmpty()) {
+        nextTrack = audioPlaylist.dequeue();
+        document.getElementById(nextTrack).play();
+    } else {
+        isAudioPlaying = false;
+    }
 }
 
-function writeToConsole(msg) {
-    console.log('changeme:'+msg)
-}
+// ********************************************************************************
+//                     | |   ___  ___  ___ <_>._ _  ___ 
+//                     | |_ / . \/ . |/ . || || ' |/ . |
+//                     |___|\___/\_. |\_. ||_||_|_|\_. |
+//                               <___'<___'        <___'                             
+// ********************************************************************************
 
 function writeDebugToConsole(msg) {
     console.log(msg)
@@ -42,3 +121,6 @@ function writeWarningToConsole(msg) {
 function writeErrorToConsole(msg) {
     console.error(msg)
 }
+
+
+
