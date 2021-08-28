66a892214b094ece2da55a9756b938e9b23d24fc
Hyper-Dragon <61525854+Hyper-Dragon@users.noreply.github.com>
2021-08-28 20:47:31 +0100

    File tidy

--- source/DynamicBoard/ShadowBoardRenderer.cs
+++ source/DynamicBoard/ShadowBoardRenderer.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Caching.Memory;
+﻿using DynamicBoard.Assets;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
