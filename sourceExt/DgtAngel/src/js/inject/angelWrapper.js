859ae9dfd11fe4336c8ca225e17c41771724496e
Hyper-Dragon <61525854+Hyper-Dragon@users.noreply.github.com>
2021-09-03 15:07:55 +0100

    refactoring

--- sourceExt/DgtAngel/src/js/inject/angelWrapper.js
+++ sourceExt/DgtAngel/src/js/inject/angelWrapper.js
@@ -1,16 +1,25 @@
 /**
  * DTG Angel Generic injection script
  *  - Calls GetRemoteBoardState() in the scrape file added by the manifest
- * 
+ *
  */
-var port = chrome.runtime.connect({name: WRAPPER_PORT_NAME});
+var port = chrome.runtime.connect({ name: WRAPPER_PORT_NAME });
 
 setInterval(() => {
     if (document.readyState === "complete") {
-        updateMsg = GetBlankMessage(WRAPPER_SOURCE_NAME,"STATE_UPDATED");
+        updateMsg = GetBlankMessage(WRAPPER_SOURCE_NAME, "STATE_UPDATED");
 
         try {
-            updateMsg.RemoteBoard = GetRemoteBoardState();
+            if (IsUrlValid(window.location.toString())) {
+                updateMsg.RemoteBoard = GetRemoteBoardState();
+            } else {
+                updateMsg.RemoteBoard = getDefaultRemoteBoard();
+                updateMsg.RemoteBoard.State.Code = "UNKNOWN_PAGE";
+                updateMsg.RemoteBoard.State.Message =
+                    "If you can see this something has changed on chess.com";
+                updateMsg.RemoteBoard.Board = null;
+                updateMsg.RemoteBoard.BoardConnection = null;
+            }
         } catch (err) {
             updateMsg.RemoteBoard = getDefaultRemoteBoard();
             updateMsg.RemoteBoard.State.Code = "PAGE_READ_ERROR";
@@ -18,16 +27,14 @@ setInterval(() => {
             updateMsg.RemoteBoard.Board = null;
             updateMsg.RemoteBoard.BoardConnection = null;
         }
-        
+
         port.postMessage({ BoardScrapeMsg: updateMsg });
-        
+
         //echo for popup
         chrome.runtime.sendMessage({ BoardScrapeMsg: updateMsg });
-    
     } else {
         console.log("Document not ready");
     }
 }, PAGE_POLL_DELAY_MS);
 
 console.log("Watching Page...");
-
