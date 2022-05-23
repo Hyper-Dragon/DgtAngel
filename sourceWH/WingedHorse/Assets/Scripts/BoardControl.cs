f7bc8a913b20aad532b55b03f69a62fcfc964fc1
HyperD <stluisreyuk@hotmail.com>
2022-05-24 00:44:56 +0100

    Camera speed + movement changes

--- sourceWH/WingedHorse/Assets/Scripts/BoardControl.cs
+++ sourceWH/WingedHorse/Assets/Scripts/BoardControl.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -13,6 +11,9 @@ public class BoardControl : MonoBehaviour
     TextMeshPro whiteText;
     TextMeshPro blackText;
     bool isWhiteOnBottom = true;
+
+    public float rotationStep = 5f;
+    public float speedUpModifier = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +30,7 @@ public class BoardControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float speedModifier = Input.GetKey(KeyCode.LeftAlt) ? 10f : 1f;
+        float speedModifier = Input.GetKey(KeyCode.Space) ? speedUpModifier : 1f;
         
         whiteClock.text = MessageHandler.WhiteClock;
         blackClock.text = MessageHandler.BlackClock;
@@ -46,15 +47,15 @@ public class BoardControl : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Q))
         {
-            board.transform.Rotate(0, -10f * speedModifier * Time.deltaTime, 0);
-            squareTextA1.transform.Rotate(0, 0, -10f * speedModifier * Time.deltaTime);
-            squareTextH8.transform.Rotate(0, 0, -10f * speedModifier * Time.deltaTime);
+            board.transform.Rotate(0, -rotationStep * speedModifier * Time.deltaTime, 0);
+            squareTextA1.transform.Rotate(0, 0, -rotationStep * speedModifier * Time.deltaTime);
+            squareTextH8.transform.Rotate(0, 0, -rotationStep * speedModifier * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.E))
         {
-            board.transform.Rotate(0, 10f * speedModifier * Time.deltaTime, 0);
-            squareTextA1.transform.Rotate(0, 0, 10f * speedModifier * Time.deltaTime);
-            squareTextH8.transform.Rotate(0, 0, 10f * speedModifier * Time.deltaTime);
+            board.transform.Rotate(0, rotationStep * speedModifier * Time.deltaTime, 0);
+            squareTextA1.transform.Rotate(0, 0, rotationStep * speedModifier * Time.deltaTime);
+            squareTextH8.transform.Rotate(0, 0, rotationStep * speedModifier * Time.deltaTime);
         }
     }
 }
--- sourceWH/WingedHorse/Assets/Scripts/CameraManager.cs
+++ sourceWH/WingedHorse/Assets/Scripts/CameraManager.cs
@@ -8,16 +8,19 @@ public class CameraManager : MonoBehaviour
     private Camera mainCamera;
     private GameObject mainLight;
     private GameObject board;
-    GameObject squareText;
+    private Renderer cameraTarget;
+    private Rigidbody cameraBody;
     private Vector3 cameraTargetPosition;
-    private Vector3 tableCentre;
     private Vector3 initialCameraPos = new(0, 0, 0);
     private Quaternion initialCameraRot = new(0, 0, 0, 0);
-    private bool isLastMoveLeft = false;
-    private bool isLastMoveUp = false;
+
     
     public int targetFrameRate = 30;
-
+    public float moveStep = 1f;
+    public float fovStep = 2f;
+    public float rotationStep = 10f;
+    public float speedUpModifier = 10f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -30,8 +33,9 @@ public class CameraManager : MonoBehaviour
         mainLight = GameObject.Find("MainLight");
 
         board = GameObject.Find("Board");
+        cameraTarget = GameObject.Find("Board/BaseTarget/Centre").GetComponent<Renderer>();
+        cameraBody = GameObject.Find("Main Camera").GetComponent<Rigidbody>();
         cameraTargetPosition = GameObject.Find("CameraTarget").transform.position;
-        tableCentre = board.transform.position;
 
         initialCameraPos = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y,
                                        mainCamera.transform.position.z);
@@ -43,118 +47,53 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float speedModifier = Input.GetKey(KeyCode.LeftAlt) ? 10f : 1f;
-
-        if (Input.GetKey(KeyCode.Space))
+        // Reset Scene
+        if (Input.GetKey(KeyCode.R))
         {
             mainCamera.transform.SetPositionAndRotation(initialCameraPos, initialCameraRot);
         }
 
-        Vector3 viewPos = mainCamera.WorldToViewportPoint(board.transform.position);
-        bool isLeftOk = true;
-        bool isRightOk = true;
-        bool isZoomInOk = true;
-        bool isZoomOutOk = true;
-        bool isUpOk = true;
-        bool isDownOk = true;
-
-        
-        if (viewPos.x <= 0 || viewPos.x >= 1 ||
-            viewPos.y <= 0 || viewPos.y >= 1 ||
-            viewPos.z <= 0)
-        {
-            if (viewPos.x <= 0.5)
-            {
-                isLeftOk = true;
-                isRightOk = false;
-                isZoomInOk = false;
-                isZoomOutOk = true;
-            }
-            else
-            {
-                isLeftOk = false;
-                isRightOk = true;
-                isZoomInOk = false;
-                isZoomOutOk = true;
-            }
-
-            if (viewPos.x <= 0.5)
-            {
-                isUpOk = true;
-                isDownOk = false;
-            }
-            else
-            {
-                isUpOk = false;
-                isDownOk = true;
-            }
-
-        }
-
-        if (Input.GetKey(KeyCode.D) && isLeftOk)
-        {
-            board.transform.position += new Vector3(0, 0, 2f * speedModifier * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.A) && isRightOk)
-        {
-            board.transform.position += new Vector3(0, 0, -2f * speedModifier * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.W) && isUpOk)
-        {
-            board.transform.position += new Vector3(-2f * speedModifier * Time.deltaTime, 0, 0);
-        }
-        else if (Input.GetKey(KeyCode.S) && isDownOk)
-        {
-            board.transform.position += new Vector3(2f * speedModifier * Time.deltaTime, 0, 0);
-        }
-
-
-        if (Input.GetKey(KeyCode.Z) && mainCamera.fieldOfView > 20 && isZoomInOk)
-        {
-            mainCamera.fieldOfView -= 5 * speedModifier * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.X) && mainCamera.fieldOfView < 70 && isZoomOutOk)
-        {
-            mainCamera.fieldOfView += 5f * speedModifier * Time.deltaTime;
-        }
+        float speedModifier = Input.GetKey(KeyCode.Space) ? speedUpModifier : 1f;
+        board.transform.position += Input.GetKey(KeyCode.D) && (cameraTarget.isVisible || cameraTarget.transform.position.z < 0) ? new Vector3(0, 0, moveStep * speedModifier * Time.deltaTime) : Vector3.zero;
+        board.transform.position += Input.GetKey(KeyCode.A) && (cameraTarget.isVisible || cameraTarget.transform.position.z > 0) ? new Vector3(0, 0, -moveStep * speedModifier * Time.deltaTime) : Vector3.zero;
+        board.transform.position += Input.GetKey(KeyCode.W) && (cameraTarget.isVisible || cameraTarget.transform.position.x > 0) ? new Vector3(-moveStep * speedModifier * Time.deltaTime, 0, 0) : Vector3.zero;
+        board.transform.position += Input.GetKey(KeyCode.S) && (cameraTarget.isVisible || cameraTarget.transform.position.x < 0) ? new Vector3(moveStep * speedModifier * Time.deltaTime, 0, 0) : Vector3.zero;
+        mainCamera.fieldOfView -= Input.GetKey(KeyCode.Z) && (cameraTarget.isVisible && mainCamera.fieldOfView > 20) ? fovStep * speedModifier * Time.deltaTime : 0;
+        mainCamera.fieldOfView += Input.GetKey(KeyCode.X) && (cameraTarget.isVisible && mainCamera.fieldOfView < 70) ? fovStep * speedModifier * Time.deltaTime : 0;
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            if (mainCamera.transform.eulerAngles.x >= 25 || !isLastMoveUp)
+            if (mainCamera.transform.forward.x < 0.5)
             {
-                mainCamera.transform.RotateAround(cameraTargetPosition, new Vector3(0, 0, 15 * Time.deltaTime), 20 * speedModifier * Time.deltaTime);
-                mainLight.transform.RotateAround(cameraTargetPosition, new Vector3(0, 0, 15 * Time.deltaTime), 20 * speedModifier * Time.deltaTime);
-                isLastMoveUp = true;
+                mainCamera.transform.RotateAround(cameraTargetPosition, new Vector3(0, 0, rotationStep * Time.deltaTime), rotationStep * speedModifier * Time.deltaTime);
+                mainLight.transform.RotateAround(cameraTargetPosition, new Vector3(0, 0, rotationStep * Time.deltaTime), rotationStep * speedModifier * Time.deltaTime);
             }
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            if (mainCamera.transform.eulerAngles.x >= 25 || isLastMoveUp)
+            if (mainCamera.transform.forward.x > -0.5)
             {
-                mainCamera.transform.RotateAround(cameraTargetPosition, new Vector3(0, 0, -15 * Time.deltaTime), 20 * speedModifier * Time.deltaTime);
-                mainLight.transform.RotateAround(cameraTargetPosition, new Vector3(0, 0, -15 * Time.deltaTime), 20 * speedModifier * Time.deltaTime);
-                isLastMoveUp = false;
+                mainCamera.transform.RotateAround(cameraTargetPosition, new Vector3(0, 0, -rotationStep * Time.deltaTime), rotationStep * speedModifier * Time.deltaTime);
+                mainLight.transform.RotateAround(cameraTargetPosition, new Vector3(0, 0, -rotationStep * Time.deltaTime), rotationStep * speedModifier * Time.deltaTime);
             }
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if (mainCamera.transform.eulerAngles.x >= 25 || !isLastMoveLeft)
+            if (mainCamera.transform.forward.z > -0.5)
             {
-                mainCamera.transform.RotateAround(cameraTargetPosition, new Vector3(15 * Time.deltaTime, 0, 0), 20 * speedModifier * Time.deltaTime);
-                mainLight.transform.RotateAround(cameraTargetPosition, new Vector3(15 *Time.deltaTime, 0, 0), 20 * speedModifier * Time.deltaTime);
-                isLastMoveLeft = true;
+                mainCamera.transform.RotateAround(cameraTargetPosition, new Vector3(rotationStep * Time.deltaTime, 0, 0), rotationStep * speedModifier * Time.deltaTime);
+                mainLight.transform.RotateAround(cameraTargetPosition, new Vector3(rotationStep * Time.deltaTime, 0, 0), rotationStep * speedModifier * Time.deltaTime);
             }
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            if (mainCamera.transform.eulerAngles.x >= 25 || isLastMoveLeft)
+            if (mainCamera.transform.forward.z < 0.5 )
             {
-                mainCamera.transform.RotateAround(cameraTargetPosition, new Vector3(-15 * Time.deltaTime, 0, 0), 20 * speedModifier * Time.deltaTime);
-                mainLight.transform.RotateAround(cameraTargetPosition, new Vector3(-15 * Time.deltaTime, 0, 0), 20 * speedModifier * Time.deltaTime);
-                isLastMoveLeft = false;
+                mainCamera.transform.RotateAround(cameraTargetPosition, new Vector3(-rotationStep * Time.deltaTime, 0, 0), rotationStep * speedModifier * Time.deltaTime);
+                mainLight.transform.RotateAround(cameraTargetPosition, new Vector3(-rotationStep * Time.deltaTime, 0, 0), rotationStep * speedModifier * Time.deltaTime);
             }
         }
     }
+
 }
