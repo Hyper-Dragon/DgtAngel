016538033593644efdb38133d3cd8a146589645e
Hyper-Dragon <61525854+Hyper-Dragon@users.noreply.github.com>
2023-10-02 03:29:30 +0100

    version no.

--- source/DgtCherub/Form1.cs
+++ source/DgtCherub/Form1.cs
@@ -60,7 +60,7 @@ namespace DgtCherub
                                                      "Ваш хід"};
 
         private const int TEXTBOX_MAX_LINES = 200;
-        private const string VERSION_NUMBER = "0.4.6-IVOR-01";
+        private const string VERSION_NUMBER = "0.4.6-IVOR-02";
         private const string PROJECT_URL = "https://hyper-dragon.github.io/DgtAngel/";
         private const int LIVE_CHESS_LISTEN_PORT = 1982;
         private const string VIRTUAL_CLOCK_PORT = "37964";
